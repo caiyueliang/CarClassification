a28e126a9fe89a2a5b2cf35e86126ea43325e352
caiyueliang <393900414@qq.com>
2018-11-22 15:59:23 +0800

    update

--- pre_process/pre_process.py
+++ pre_process/pre_process.py
@@ -1,7 +1,31 @@
 # encoding:utf-8
+import os
+import shutil
+
+
+def mkdir_if_not_exist(path):
+    if not os.path.exists(os.path.join(path)):
+        os.makedirs(os.path.join(path))
+
 
 def data_pre_process(root_path, input_path):
+    files_list = list()
 
+    for root, dirs, files in os.walk(root_path):
+        for file in files:
+            # print(file)     #文件名
+            files_list.append(os.path.join(root, file))
+
+    mkdir_if_not_exist(input_path)
+
+    for file in files_list:
+        print(file)
+        name_list = file.split('/')[-1].split('_')
+        # print(name_list)
+        mkdir_if_not_exist(os.path.join(input_path, (name_list[0] + "_" + name_list[1]).replace(' ', '')))
+        shutil.copy(file, os.path.join(input_path, (name_list[0] + "_" + name_list[1]).replace(' ', ''), name_list[2]))
+
+    print len(files_list)
     return
 
 
