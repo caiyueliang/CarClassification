7b2f0e1bba670ce563a01d7faf15fe0e264001b4
lijinchao2007 <lijinchao2007@163.com>
2018-11-22 15:21:42 +0800

    update

---
+++ pre_process/pre_process.py
@@ -0,0 +1,9 @@
+# encoding:utf-8
+
+def data_pre_process(root_path, input_path):
+
+    return
+
+
+if __name__ == '__main__':
+    data_pre_process('../../Data/car_classifier', '../../Data/car_classifier_new')
