088d415f0fe4710f1051267bb5d1d0d83f624f42
lijinchao2007 <lijinchao2007@163.com>
2018-11-26 16:39:51 +0800

    update

--- head_tail_classifier/head_tail_classifier.py
+++ head_tail_classifier/head_tail_classifier.py
@@ -4,11 +4,11 @@ import model_train
 from torchvision import models
 
 if __name__ == '__main__':
-    train_path = '../Data/car_classifier_train/train'
-    test_path = '../Data/car_classifier_train/test'
+    train_path = '../../Data/head_tail_classifier/train'
+    test_path = '../../Data/head_tail_classifier/test'
 
-    FILE_PATH = './model/resnet18_params.pkl'
-    num_classes = 356
+    FILE_PATH = './model/htc_squeezenet.pkl'
+    num_classes = 2
     batch_size = 100
     img_size = 224
     lr = 1e-2
@@ -19,7 +19,8 @@ if __name__ == '__main__':
     print('img_size: %d' % img_size)
     print('batch_size: %d' % batch_size)
 
-    model = models.resnet18(num_classes=num_classes)
+    # model = models.resnet18(num_classes=num_classes)
+    model = models.squeezenet1_1(num_classes=num_classes)
     model_train = model_train.ModuleTrain(train_path, test_path, FILE_PATH, model=model, batch_size=batch_size,
                                           img_size=img_size, lr=lr)
 
