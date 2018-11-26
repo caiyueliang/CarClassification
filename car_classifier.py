4adadab3d6a5c9830c1cd1a1105e25fbb1e846d4
lijinchao2007 <lijinchao2007@163.com>
2018-11-26 17:00:25 +0800

    update

--- car_classifier.py
+++ car_classifier.py
@@ -1,26 +1,47 @@
 # coding=utf-8
+from argparse import ArgumentParser
 import os
 import model_train
 from torchvision import models
 
-if __name__ == '__main__':
-    train_path = '../Data/car_classifier_train/train'
-    test_path = '../Data/car_classifier_train/test'
 
-    FILE_PATH = './model/resnet18_params.pkl'
-    num_classes = 356
-    batch_size = 100
-    img_size = 224
-    lr = 1e-2
+def parse_argvs():
+    parser = ArgumentParser(description='car_classifier')
+    parser.add_argument('--train_path', type=str, help='train dataset path', default='../Data/car_classifier_train/train')
+    parser.add_argument('--test_path', type=str, help='test dataset path', default='../Data/car_classifier_train/test')
+
+    parser.add_argument("--output_model_path", type=str, help="output model path", default='./model/resnet18_params.pkl')
+    parser.add_argument('--classes_num', type=int, help='classes num', default=356)
+    parser.add_argument('--batch_size', type=int, help='batch size', default=64)
+    parser.add_argument('--img_size', type=int, help='imgsize', default=224)
+    parser.add_argument('--lr', type=float, help='learning rate', default=0.01)
+
+    input_args = parser.parse_args()
+    print(input_args)
+    return input_args
+
+
+if __name__ == '__main__':
+    args = parse_argvs()
+
+    train_path = args.train_path
+    test_path = args.test_path
+    output_model_path = args.output_model_path
+    num_classes = args.classes_num
+    batch_size = args.batch_size
+    img_size = args.img_size
+    lr = args.lr
 
     print('train_path: %s' % train_path)
     print('test_path: %s' % test_path)
+    print('output_model_path: %s' % output_model_path)
     print('num_classes: %d' % num_classes)
     print('img_size: %d' % img_size)
     print('batch_size: %d' % batch_size)
+    print('lr: %s' % lr)
 
     model = models.resnet18(num_classes=num_classes)
-    model_train = model_train.ModuleTrain(train_path, test_path, FILE_PATH, model=model, batch_size=batch_size,
+    model_train = model_train.ModuleTrain(train_path, test_path, output_model_path, model=model, batch_size=batch_size,
                                           img_size=img_size, lr=lr)
 
     model_train.train(200, 60)
---
+++ train.sh
@@ -0,0 +1,6 @@
+python car_classifier.py
+python car_classifier.py
+python car_classifier.py
+
+python car_classifier.py --lr 0.001
+python car_classifier.py --lr 0.001
