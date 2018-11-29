a5369184ae24ada7a6a696f11cf8a251c9ebad47
lijinchao2007 <lijinchao2007@163.com>
2018-11-29 16:39:42 +0800

    update

--- head_tail_classifier/model_train.py
+++ head_tail_classifier/model_train.py
@@ -47,6 +47,7 @@ class ModuleTrain:
         self.transform_train = T.Compose([
             T.RandomHorizontalFlip(),
             T.RandomResizedCrop(size=self.img_size, scale=(0.08, 1.0), ratio=(3./4., 4./3.)),
+            T.RandomRotation(10),
             T.ToTensor(),
             T.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5]),
         ])
